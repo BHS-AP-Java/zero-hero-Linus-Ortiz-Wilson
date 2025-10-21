8e4b6829be992b7ad34b077d40e75e583cb16ba6
Linus O <1116892@apps.nsd.org>
2025-10-21 16:26:38 +0000

    I made an app file!

---
+++ src/main/java/edu/bhscs/App.java
@@ -0,0 +1,9 @@
+package edu.bhscs;
+
+public class App {
+  public void main(String[] args){
+
+    System.out.println("im app!");
+  }
+  
+}
--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -22,11 +22,9 @@ public class Main {
     System.out.println("burger");
   }
 
-  /*
-   * public static String breadGuts(int width) { String guts = ""; for (int i = 0; i < width; i++) {
-   * guts += "' .  . '   . ' "; } return guts; }
-   *
-   * public static void draw() { System.out.println("big line   |" + breadGuts(3) + "|");
-   */
+
+
+
+
 
 }
